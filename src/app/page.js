fdf340957c9a028b61adc81b03c340755c76595d
dohehehe <doheerlob@gmail.com>
2026-09-18 18:00:10 +0900

    feat: add showSectionLabel prop to PostList and PostCard components for conditional section display

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default async function Home() {
 
   return (
     <main className={styles.main}>
-      <PostList posts={posts} />
+      <PostList posts={posts} showSectionLabel />
     </main>
   );
 }
